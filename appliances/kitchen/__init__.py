3e958bc8c69efa33318347333749d6ce23f1270c
Ryan Bishop <rdbishop06@gmail.com>
2020-01-20 14:52:38 -0600

    fix: made the Kessel Run in 12 parsecs

--- appliances/kitchen/__init__.py
+++ appliances/kitchen/__init__.py
@@ -1,2 +1,6 @@
 from .can_opener import CanOpener
 from .coffeemaker import CoffeeMaker
+from .stove import Stove
+
+from .utility.dishwasher import DishWasher
+from .utility.refrigerator import Refrigerator
